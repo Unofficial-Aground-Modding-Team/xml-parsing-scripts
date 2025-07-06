1a8b644ab7cc5027f4518acbecf3170137cfe4de
etrotta <etrotta@duck.com>
2025-07-06 16:04:33 -0300

    add FPS control

--- notebooks/interactive_render.py
+++ notebooks/interactive_render.py
@@ -21,12 +21,16 @@ def _():
 
 @app.cell
 def _(mo):
-    width_slider = mo.ui.slider(64, 1024, 16, value=256, show_value=True, include_input=True)
-    height_slider = mo.ui.slider(64, 1024, 16, value=256, show_value=True, include_input=True)
+    width_slider = mo.ui.slider(64, 1024, 16, value=256, include_input=True)
+    height_slider = mo.ui.slider(64, 1024, 16, value=256, include_input=True)
+    fps_slider = mo.ui.slider(5, 60, 1, value=15, include_input=True)
     mo.vstack(
-        [mo.hstack(["Width:", width_slider], justify="start"), mo.hstack(["Height:", height_slider], justify="start")]
+        [mo.hstack(["Width:", width_slider], justify="start"),
+         mo.hstack(["Height:", height_slider], justify="start"),
+         mo.hstack(["FPS:", fps_slider], justify="start"),
+        ]
     )
-    return height_slider, width_slider
+    return fps_slider, height_slider, width_slider
 
 
 @app.cell
@@ -43,11 +47,16 @@ def _(HEIGHT, Image, WIDTH):
 
 
 @app.cell
-def _(DataContainer, HEIGHT, Stage, WIDTH):
+def _(DataContainer):
     with open("clean/parsed.json", "r") as _file:
         data = DataContainer.model_validate_json(_file.read())
+    return (data,)
+
+
+@app.cell
+def _(HEIGHT, Stage, WIDTH, data):
     stage = Stage(width=WIDTH, height=HEIGHT, data=data)
-    return data, stage
+    return (stage,)
 
 
 @app.cell
@@ -69,8 +78,8 @@ def _(data, mo, search_box):
     tile_choices = [tile for tile in data.tiles if all(part in tile for part in _query)]
     animation_choices = [anim for anim in data.animations if all(part in anim for part in _query)]
 
-    tile_selection = mo.ui.dropdown(options=tile_choices, allow_select_none=True, value=None)
-    animation_selection = mo.ui.dropdown(options=["single"] + animation_choices, value="single")
+    tile_selection = mo.ui.dropdown(options=sorted(tile_choices, key=len), allow_select_none=True, value=None)
+    animation_selection = mo.ui.dropdown(options=["single"] + sorted(animation_choices, key=len), value="single")
     mo.vstack(
         [
             tile_selection,
@@ -88,7 +97,13 @@ def _(animation_selection, tile_selection):
 
 
 @app.cell
-def _(animation_id, background, data, mo, stage, tile_id):
+def _(animation_id, data):
+    data.animations[animation_id]
+    return
+
+
+@app.cell
+def _(animation_id, background, data, fps_slider, mo, stage, tile_id):
     gif = []
     mo.stop(tile_id is None or animation_id is None)
     _anim = data.animations[animation_id]
@@ -98,7 +113,7 @@ def _(animation_id, background, data, mo, stage, tile_id):
         stage.render(tile_id, animation_id, index=_index)
         gif.append(stage.image)
 
-    gif[0].save("tmp.gif", save_all=True, append_images=gif[1:], loop=0, duration=1000 / 15)
+    gif[0].save("tmp.gif", save_all=True, append_images=gif[1:], loop=0, duration=1000 / fps_slider.value)
     mo.image(open("tmp.gif", "rb"))
     return
 
